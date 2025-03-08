50d1e86dec2622f6d4854e4f81d9c9e7c688d8aa
mackgeofries <mark.jacobi@gmail.com>
2025-03-08 06:18:18 -0600

    first commit

---
+++ 004_offsets_and_lists.py
@@ -0,0 +1,9 @@
+        # Lesson 32 - Understanding the Offset and Appending Items to Lists
+
+       
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "etc..."] # creating a list
+
+ 
+
+print(states_of_america[0]) # pull first item from list
