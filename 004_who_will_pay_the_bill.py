a7594ec54c8b325196b0e018c3b76eb45cbf5eaa
mackgeofries <mark.jacobi@gmail.com>
2025-03-08 06:52:45 -0600

    adding additonal notes

--- 004_who_will_pay_the_bill.py
+++ 004_who_will_pay_the_bill.py
@@ -16,3 +16,5 @@ random_number = random.randint(1, len(friends)) - 1  # decided to do it this way
 print(f"{friends[random_number]} has to pay the bill today.")
 
 # after watching solution, random.choose(friends) seems like the easier solution I should have probably used instead.
+# she lists random.choose() as option 1, or random.randint(0,4) as option 2 (because there's 5 places), so I guess I
+# was "right", but did a more complicated version of it.
