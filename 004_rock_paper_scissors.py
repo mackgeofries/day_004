ae0e669c0f37d97a6360e33129a9e10f8694cd7b
mackgeofries <mark.jacobi@gmail.com>
2025-03-08 08:53:39 -0600

    added notes after watching solution

--- 004_rock_paper_scissors.py
+++ 004_rock_paper_scissors.py
@@ -5,6 +5,8 @@ import random
 # paper = "\n    _______\n---'   ____)____\n          ______)\n          _______)\n         _______)\n---.__________)"
 # rock = "\n    _______\n---'   ____)\n      (_____)\n      (_____)\n      (____)\n---.__(___)"
 # scissors = "\n    _______\n---'   ____)____\n          ______)\n       __________)\n      (____)\n---.__(___)"
+
+# rps -> rock paper scissors
 rps = ["\n    _______\n---'   ____)\n      (_____)\n      (_____)\n      (____)\n---.__(___)",  "\n    _______\n---'   ____)____\n          ______)\n          _______)\n         _______)\n---.__________)",  "\n    _______\n---'   ____)____\n          ______)\n       __________)\n      (____)\n---.__(___)"]
 
 player_choice = int(input("What will you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n "))
@@ -37,3 +39,19 @@ elif player_choice == 0:
         print("\nYou win")
     elif computer_choice == 1:
         print("\nYou lose")
+
+# I forgot about block notes, which would have made it a lot more obvious which one was which.
+# so, for example...
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+# etc...
+#
+# and then doing...
+# rps = [rock, paper, scissors].
+# and (I think) continuing on from there.
